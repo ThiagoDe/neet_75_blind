acf4b135e5e33c6d884d775792cd5ba336f60b9c
ThiagoDe <17tawi@gmail.com>
2022-07-20 23:03:59 -0700

    Is anagram

--- arrays_and_hashing/contains_duplicate.py
+++ arrays_and_hashing/contains_duplicate.py
@@ -6,3 +6,14 @@ def containsDuplicate(self, nums):
         count_n[n] = True
 
     return False
+
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         hashset = set()
+
+#         for n in nums:
+#             if n in hashset:
+#                 return True
+#             hashset.add(n)
+#         return False
