dadefa23498474ecc97742c04f6603377962ef4e
ThiagoDe <17tawi@gmail.com>
2022-07-20 23:18:39 -0700

    Two sum

--- arrays_and_hashing/two_sum.py
+++ arrays_and_hashing/two_sum.py
@@ -1,7 +1,8 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    indices = {}
-    for i in range(len(nums)):
-        dif = target - nums[i]
-        if dif in indices:
-            return [indices[dif], i]
-        indices[nums[i]] = i
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        indices = {}
+        for i in range(len(nums)):
+            dif = target - nums[i]
+            if dif in indices:
+                return [indices[dif], i]
+            indices[nums[i]] = i
