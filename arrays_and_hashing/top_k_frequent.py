82785d9cfa2c394866d46b11c0b8a2601975e8bb
ThiagoDe <17tawi@gmail.com>
2022-07-23 15:47:12 -0700

    Top k frequent

---
+++ arrays_and_hashing/top_k_frequent.py
@@ -0,0 +1,28 @@
+# Bucket sort algorithm 
+
+nums = [1,1,1,2,2,3]
+k = 2
+# Output: [1,2]
+
+def topKFrequent(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]  # [ [],[],[],[],[],[] ]
+
+    for n in nums:
+        count[n] = 1 + count.get(n, 0) # get garantee default 0
+    
+    for n, c in count.items():
+        freq[c].append(n)
+
+    f = len(freq) - 1
+    res = []
+    while len(res) < k:
+        if not freq[f]:
+            f -= 1
+        else:
+           last = freq[f].pop()
+           res.append(last)
+
+    return res 
+
+print(topKFrequent(nums, k))
