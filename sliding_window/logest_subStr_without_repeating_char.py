767b31fe6519a8aecb33910bf93701b3ae4b5572
ThiagoDe <17tawi@gmail.com>
2022-07-24 11:32:45 -0700

    Longest Substring without repeating

--- sliding_window/logest_subStr_without_repeating_char.py
+++ sliding_window/logest_subStr_without_repeating_char.py
@@ -7,12 +7,13 @@ def lengthOfLongestSubstring(s):
     res = 0
 
     for r in range(len(s)):
-        while s[r] in charSet:
+        while s[r] in charSet: # d
             charSet.remove(s[l])
             l += 1
-        charSet.add(s[r])
-        res = max(res, (r - l + 1))
-    return res 
+        print(s[r])
+        charSet.add(s[r]) # add (set)
+        res = max(res, (r - l + 1)) # 0 - 0 + 1, 1 - 0 + 1, 2 - 1 + 1, 3 - 1 + 1
+    return res # 1, 2, 2, 
         
 
 
