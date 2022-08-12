2a0f75674e6620b9e117606edaa3eb98eb8b4882
ThiagoDe <17tawi@gmail.com>
2022-08-12 12:38:21 -0700

    top k frequent reviewed

--- arrays_and_hashing/top_k_frequent.py
+++ arrays_and_hashing/top_k_frequent.py
@@ -1,6 +1,7 @@
 # Bucket sort algorithm 
 
-nums = [1,1,1,2,2,3]
+nums = [1,1,1,2,2,3] # best solution is to use the length on the input to create buckets
+# each sub arr represent the num of times that the number occured  
 k = 2
 # Output: [1,2]
 
@@ -11,8 +12,9 @@ def topKFrequent(nums, k):
     for n in nums:
         count[n] = 1 + count.get(n, 0) # get garantee default 0
     
-    for n, c in count.items():
-        freq[c].append(n)
+    # print(count.items())
+    for n, c in count.items(): # gives [(k,v)] list of tuples from the dictionary 
+        freq[c].append(n) # c is number of ocurrences and n is the actual number
 
     f = len(freq) - 1
     res = []
@@ -22,6 +24,21 @@ def topKFrequent(nums, k):
         else:
            last = freq[f].pop()
            res.append(last)
+
+    # count = {}
+    # freq = [[] for i in range(len(nums) + 1)]
+
+    # for n in nums:
+    #     count[n] = 1 + count.get(n, 0)
+    # for n, c in count.items():
+    #     freq[c].append(n)
+
+    # res = []
+    # for i in range(len(freq) - 1, 0, -1):
+    #     for n in freq[i]:
+    #         res.append(n)
+    #         if len(res) == k:
+    #             return res
 
     return res 
 
