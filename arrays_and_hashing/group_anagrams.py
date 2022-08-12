e970e556081d52d2db4b93198d74073053c59ffd
ThiagoDe <17tawi@gmail.com>
2022-08-12 10:57:01 -0700

    group anagrams review

--- arrays_and_hashing/group_anagrams.py
+++ arrays_and_hashing/group_anagrams.py
@@ -1,7 +1,24 @@
 from collections import defaultdict
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+# Example 1:
+
+# Input: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+# Output: [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+# Example 2:
+
+# Input: strs = [""]
+# Output: [[""]]
+# Example 3:
+
+# Input: strs = ["a"]
+# Output: [["a"]]
 
 def groupAnagrams(strs):
-    res = defaultdict(list)
+    res = defaultdict(list) # to deal w edge case the dict = { key: [] } list default
     # for s in strs:
     #     # print(s)
     #     sortedS = ''.join(sorted(s))
@@ -12,13 +29,18 @@ def groupAnagrams(strs):
     #         res[sortedS].append(s)
 
     # return res.values()
-    for s in strs:
-        count = [0] * 26
+    for s in strs: #O(m * n)
+        count = [0] * 26 # count the char a-z (122 - 97)
+        # [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
         for c in s:
             count[ord(c) - ord('a')] += 1
-
-        res[tuple(count)].append(s)
-
+            # print(ord(c))
+            # print(ord('a'))
+            # print(ord('z'))
+            # print(count)
+            print(tuple(count))
+        res[tuple(count)].append(s) # in python list can not be keys
+        print(count)
     return res.values()
 
 strs = ["eat","tea","tan","ate","nat","bat"]
