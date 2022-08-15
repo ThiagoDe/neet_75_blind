8679a650dbf8623fc7a3b49d62e6322c8e0c201b
ThiagoDe <17tawi@gmail.com>
2022-08-14 21:54:11 -0700

    Longest consecutive sequence reviewed

--- arrays_and_hashing/longest_consecutive_sequence.py
+++ arrays_and_hashing/longest_consecutive_sequence.py
@@ -2,13 +2,13 @@ nums = [100, 4, 200, 1, 3, 2]
 # Output: 4
 
 def longestConsecutive(nums):
-    numSet = set(nums)
+    numSet = set(nums) # the set is the most efficent way to check if num exist 
     longest = 0
 
     for n in nums:
-        if (n - 1) not in numSet:
-            length = 0
-            while (n + length) in numSet:
+        if (n - 1) not in numSet: # means that this is the first num of a sequense 
+            length = 1
+            while (n + length) in numSet: # find how many adjents in the sequence 
                 length += 1
             longest = max(longest, length)
 
