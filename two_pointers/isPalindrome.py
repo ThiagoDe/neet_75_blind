581143cac6b8789d465640fc1cf028a1020f6eac
ThiagoDe <17tawi@gmail.com>
2022-08-15 12:05:04 -0700

    Palindrome constant time memory two pointers approch

--- two_pointers/isPalindrome.py
+++ two_pointers/isPalindrome.py
@@ -1,7 +1,7 @@
 s = "A man, a plan, a canal: Panama"
 
 
-class Solution:
+class Solution: # contant memory O(n)
 
     def isPalindrome(self, s: str) -> bool:
         i = 0
@@ -9,9 +9,9 @@ class Solution:
         # print(self.isAlpha(s[j]))
         # print(self.isAlpha(s[i]))
         while i < j:
-            while i < j and self.isAlpha(s[j]) == False:
+            while i < j and self.isAlpha(s[j]) == False: #garanty to scape non alphnum
                 j -= 1
-            while i < j and self.isAlpha(s[i]) == False:
+            while i < j and self.isAlpha(s[i]) == False: #garanty to scape non alphnum
                 i += 1
             if s[i].lower() != s[j].lower():
                 print(s[i])
@@ -23,7 +23,7 @@ class Solution:
 
         return True
 
-    def isAlpha(self, ch: str):
+    def isAlpha(self, ch: str): # function check if the character are alphanumerical
         return (ord('A') <= ord(ch) <= ord('Z') or
                 ord('a') <= ord(ch) <= ord('z') or
                 ord('0') <= ord(ch) <= ord('9'))
@@ -32,3 +32,27 @@ print(Solution().isPalindrome(s))
 # print(isApha('z'))
 # print(isApha(' '))
 # print(isApha(','))
+
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         i, j = 0, len(s) - 1
+
+#         while i < j:
+#             while i < j and not self.isAlpha(s[i]):
+#                 i += 1
+#             while i < j and not self.isAlpha(s[j]):
+#                 j -= 1
+
+#             if s[i].lower() != s[j].lower():
+#                 return False
+#             else:
+#                 i += 1
+#                 j -= 1
+
+#         return True
+
+#     def isAlpha(self, c):
+#         return (ord('a') <= ord(c) <= ord('z') or
+#                 ord('A') <= ord(c) <= ord('Z') or
+#                 ord('0') <= ord(c) <= ord('9'))
