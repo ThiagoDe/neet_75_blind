382ad7873e3b99736fc72bc2ef1bd169d20241e8
ThiagoDe <17tawi@gmail.com>
2022-08-16 13:09:36 -0700

    Longest repeating sequence of chars

--- sliding_window/longest_repeating_char_replace.py
+++ sliding_window/longest_repeating_char_replace.py
@@ -6,10 +6,10 @@ def characterReplacement(s, k):
     res = 0
 
     l = 0
-    for r in range(len(s)):
-        count[s[r]] = 1 + count.get(s[r], 0)
-        while (r - l + 1) - max(count.values()) > k:
-            count[s[l]] -= 1
+    for r in range(len(s)): # range starts at 0
+        count[s[r]] = 1 + count.get(s[r], 0) # use s[r] track num chars 
+        while (r - l + 1) - max(count.values()) > k: # if k not enought to replace char
+            count[s[l]] -= 1 #update count of chars when swiching l += 1
             l += 1
         
         res = max(res, r - l + 1)
