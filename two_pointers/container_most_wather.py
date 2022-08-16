b27180fc8fdbf939f63c8169ed8b97077b45132d
ThiagoDe <17tawi@gmail.com>
2022-08-15 21:30:37 -0700

    Container most wather using two poiters

--- two_pointers/container_most_wather.py
+++ two_pointers/container_most_wather.py
@@ -1,12 +1,12 @@
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 # Output: 49
 
-def maxArea(height):
+def maxArea(height): #linear time solution O(n)
     res = 0
     l, r = 0, len(height) - 1
 
     while l < r:
-        area = (r - l) * min(height[l], height[r])
+        area = (r - l) * min(height[l], height[r]) # area cal r - l * min height
         res = max(area, res)
         if height[l] > height[r]:
             r -= 1
