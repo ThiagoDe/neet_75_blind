cff952c80e3048c01044720790dcaf3fcf40a1e8
ThiagoDe <17tawi@gmail.com>
2022-08-17 16:15:47 -0700

    reverse linked list reviewed

--- linked_list/reverse_linked_list.py
+++ linked_list/reverse_linked_list.py
@@ -3,10 +3,10 @@ class Solution:
         prev = None
         current = head
         while current:
-            nextN = current.next
-            current.next = prev
-            prev = current
+            nextN = current.next # temp val to save next
+            current.next = prev # reasign next to prev
+            prev = current # prev is now current for the next iteration
 
-            current = nextN
+            current = nextN # current is the next saved in a temporary variable
 
         return prev
