dae74c7cff2530537dfffd025d1ba4404a5d635d
ThiagoDe <17tawi@gmail.com>
2022-08-18 14:27:54 -0700

    Reverse list better explaned

--- linked_list/reverse_linked_list.py
+++ linked_list/reverse_linked_list.py
@@ -1,12 +1,17 @@
 class Solution:
-    def reverseList(self, head):
-        prev = None
+    def reverseList(self, head):  # 1 -> 2 -> 3 -> 4 -> 5 ->
+                            #prv  curr tpNxt
+        # start with two pointers prev and current 
+        prev = None 
         current = head
-        while current:
-            nextN = current.next # temp val to save next
-            current.next = prev # reasign next to prev
-            prev = current # prev is now current for the next iteration
 
-            current = nextN # current is the next saved in a temporary variable
+        #iterate through the entire list
+        # before reasign current.next 
+        while current: # 1 
+            nextN = current.next # temp val to save next = 2
+            current.next = prev # reasign next to prev  break -> 2 | -> None
+            prev = current # prev is now current for the next iteration prev = 1 
+
+            current = nextN # current is the next saved in a temporary variable current = 2
 
         return prev
