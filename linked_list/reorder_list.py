408ad881d726abfd932588669c3b5baea12b5846
ThiagoDe <17tawi@gmail.com>
2022-08-17 18:21:42 -0700

    Reorder list two poiters

---
+++ linked_list/reorder_list.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        slow.next = prev = None
+
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        first, second = head, prev
+
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
