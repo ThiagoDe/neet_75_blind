8fbc600f553dd2c55aeae13dc138ca64ab451f12
ThiagoDe <17tawi@gmail.com>
2022-08-17 17:42:15 -0700

    Merge two lists acendent order

--- linked_list/merge_two_lists.py
+++ linked_list/merge_two_lists.py
@@ -6,18 +6,19 @@ class ListNode:
         
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode()
-        tail = dummy
+        dummy = ListNode() # used to append will be returned at the end
+        tail = dummy # will change everytime tail is set to tail.next
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
+        while l1 and l2: # both have to exist 
+            if l1.val < l2.val: # acd
+                tail.next = l1  # 
+                l1 = l1.next #update l1
             else:
                 tail.next = l2
                 l2 = l2.next
             tail = tail.next
 
+        # make sure if we have linked List of diff lengths that the rest will be added to the and of the new list
         if l1:
             tail.next = l1
         elif l2:
