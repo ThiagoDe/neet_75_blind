2f805bec52909e5feadd73b5ba691794e46d6ed2
ThiagoDe <17tawi@gmail.com>
2022-10-24 15:37:48 -0700

    Longest sequence using set just check if the neighbor -1 exist to indentify the begining of the seq if yes count + 1

--- arrays_and_hashing/longest_consecutive_sequence.py
+++ arrays_and_hashing/longest_consecutive_sequence.py
@@ -15,3 +15,17 @@ def longestConsecutive(nums):
     return longest 
 
 print(longestConsecutive(nums))
+
+
+def longestConsecutive(self, nums) -> int:
+    numsSet = set(nums)
+    longest = 0
+
+    for n in nums:
+        if (n - 1) not in numsSet:
+            count = 0
+            while (n + count) in numsSet:
+                count += 1
+            longest = max(longest, count)
+
+    return longest
