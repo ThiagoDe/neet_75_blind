5d05125c3a9e3694dffa532ec842ed404194824b
ThiagoDe <17tawi@gmail.com>
2022-10-24 13:40:23 -0700

    kth element the secret if to use buckets that will containg the total times that a number appers from a hash map

--- arrays_and_hashing/top_k_frequent.py
+++ arrays_and_hashing/top_k_frequent.py
@@ -1,46 +1,72 @@
-# Bucket sort algorithm 
+# # Bucket sort algorithm 
 
-nums = [1,1,1,2,2,3] # best solution is to use the length on the input to create buckets
-# each sub arr represent the num of times that the number occured  
-k = 2
-# Output: [1,2]
+# nums = [1,1,1,2,2,3] # best solution is to use the length on the input to create buckets
+# # each sub arr represent the num of times that the number occured  
+# k = 2
+# # Output: [1,2]
 
-def topKFrequent(nums, k):
+# def topKFrequent(nums, k):
+#     count = {}
+#     freq = [[] for n in range(len(nums) + 1)]  # [ [],[],[],[],[],[] ]
+
+#     for n in nums:
+#         count[n] = 1 + count.get(n, 0) # get garantee default 0
+    
+#     # print(count.items())
+#     for n, c in count.items(): # gives [(k,v)] list of tuples from the dictionary 
+#         freq[c].append(n) # c is number of ocurrences and n is the actual number
+
+#     f = len(freq) - 1
+#     res = []
+#     while len(res) < k:
+#         if not freq[f]:
+#             f -= 1
+#         else:
+#            last = freq[f].pop()
+#            res.append(last)
+
+
+#     # count = {}
+#     # freq = [[] for i in range(len(nums) + 1)]
+
+#     # for n in nums:
+#     #     count[n] = 1 + count.get(n, 0)
+#     # for n, c in count.items():
+#     #     freq[c].append(n)
+
+#     # res = []
+#     # for i in range(len(freq) - 1, 0, -1):
+#     #     for n in freq[i]:
+#     #         res.append(n)
+#     #         if len(res) == k:
+#     #             return res
+
+#     return res 
+
+# print(topKFrequent(nums, k))
+
+
+def topKFrequent(self, nums, k: int):
+     # store in a hashmap number of ocurances
     count = {}
-    freq = [[] for n in range(len(nums) + 1)]  # [ [],[],[],[],[],[] ]
+
+    # list of sub list that goes from zero to len(list) + 1
+    freq = [[] for i in range(len(nums) + 1)]
 
     for n in nums:
-        count[n] = 1 + count.get(n, 0) # get garantee default 0
-    
-    # print(count.items())
-    for n, c in count.items(): # gives [(k,v)] list of tuples from the dictionary 
-        freq[c].append(n) # c is number of ocurrences and n is the actual number
+        count[n] = 1 + count.get(n, 0)
 
-    f = len(freq) - 1
+    # iterate hashmap val are going to be the index of the list above, append the key at the index bucket
+    for c, v in count.items():
+        freq[v].append(c)
+
+    # set res variable to list
     res = []
-    while len(res) < k:
-        if not freq[f]:
-            f -= 1
-        else:
-           last = freq[f].pop()
-           res.append(last)
 
-
-    # count = {}
-    # freq = [[] for i in range(len(nums) + 1)]
-
-    # for n in nums:
-    #     count[n] = 1 + count.get(n, 0)
-    # for n, c in count.items():
-    #     freq[c].append(n)
-
-    # res = []
-    # for i in range(len(freq) - 1, 0, -1):
-    #     for n in freq[i]:
-    #         res.append(n)
-    #         if len(res) == k:
-    #             return res
-
-    return res 
-
-print(topKFrequent(nums, k))
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            # for each sublist pop last element and push to res
+            res.append(n)
+            if len(res) == k:
+                return res
+    # # if lenght of res = to k return res
