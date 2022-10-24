36538efc446466a9f7fef4b258b06b32ff35cf2e
ThiagoDe <17tawi@gmail.com>
2022-10-24 16:55:21 -0700

    sort input array, iterate it, if prev is equal continue to next iteration now use two pointers if a + b + c > o move left pointer else move right equal push to result

--- two_pointers/3sum.py
+++ two_pointers/3sum.py
@@ -49,3 +49,30 @@ def threeSum(nums):
     return res
 
 print(threeSum(nums))
+
+
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         nums.sort()
+#         print(nums)
+#         for i, a in enumerate(nums):
+#             if i > 0 and a == nums[i - 1]:
+#                 continue
+
+#             l, r = i + 1, len(nums) - 1
+
+#             # two sum
+#             while l < r:
+#                 total = a + nums[l] + nums[r]
+#                 if total > 0:
+#                     r -= 1
+#                 elif total < 0:
+#                     l += 1
+#                 else:
+#                     res.append([a, nums[l], nums[r]])
+#                     l += 1
+#                     while nums[l] == nums[l - 1] and l < r:
+#                         l += 1
+
+#         return res
