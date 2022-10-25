07a9dd31f760b4a39d61f6bb688b45638fcb17e7
ThiagoDe <17tawi@gmail.com>
2022-10-24 17:04:37 -0700

    area is r - 1 multiplied by min hight

--- two_pointers/container_most_wather.py
+++ two_pointers/container_most_wather.py
@@ -16,3 +16,20 @@ def maxArea(height): #linear time solution O(n)
     return res 
 
 print(maxArea(height))
+
+
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         res = 0
+#         l, r = 0, len(height) - 1
+
+#         while l < r:
+#             area = (r - l) * min(height[l], height[r])
+#             res = max(res, area)
+
+#             if height[l] < height[r]:
+#                 l += 1
+#             else:
+#                 r -= 1
+
+#         return res
