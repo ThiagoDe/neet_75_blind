6681d823a768c0f18aba1e6f31faceeea22d96de
ThiagoDe <17tawi@gmail.com>
2022-10-25 12:12:01 -0700

    Use a queue for bfs remember to create list if length of res = to l else just append res[l] the node val

--- trees/tree_level_order_traversal.py
+++ trees/tree_level_order_traversal.py
@@ -1,3 +1,4 @@
+from collections import deque
 from collections import deque 
 
 def levelOrder(root):
@@ -17,3 +18,36 @@ def levelOrder(root):
                 queue.append([node.right, level + 1])
 
     return res 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
+#         res = []
+
+#         queue = deque([(root, 0)])
+
+#         while queue:
+#             node, l = queue.popleft()
+
+#             if len(res) == l:
+#                 res.append([node.val])
+#             else:
+#                 res[l].append(node.val)
+
+#             if node.left:
+#                 queue.append((node.left, l + 1))
+
+#             if node.right:
+#                 queue.append((node.right, l + 1))
+
+#         return res
