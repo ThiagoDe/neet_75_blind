9ccf3f2b027a32462d7b3b1ca92308eba7b47600
ThiagoDe <17tawi@gmail.com>
2022-10-25 11:55:38 -0700

    Lowest ancestor nlogn if is greater than both vals look to the curr.left elif smaller than both val traverse the right side of the tree else you found the lowest common ancestor, just return the current

--- trees/lowest_common_ancestor.py
+++ trees/lowest_common_ancestor.py
@@ -11,3 +11,26 @@ class Solution:
                 current = current.left
             else:
                 return current
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         curr = root
+
+#         while curr:
+#             if curr.val < p.val and curr.val < q.val:
+#                 # right side
+#                 curr = curr.right
+#             elif curr.val > p.val and curr.val > q.val:
+#                 # left side traverse
+#                 curr = curr.left
+
+#             else:
+#                 return curr
